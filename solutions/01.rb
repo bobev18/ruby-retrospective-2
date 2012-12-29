9fe37f268bd00621bf3cc30fa7d44a8ab9e581b8
Iliyan Bobev <bobev18@gmail.com>
2012-12-29 20:04:14 +0200

    more ajustments on 01

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,5 +1,6 @@
 class Integer
   def prime_divisors
+    # Changed to make it easier to understand
     num = self.abs
     result, div = [],1
     while num>1
@@ -13,30 +14,22 @@ end
 
 class Range
   def fizzbuzz
-    collect do |z|
-      if z%3==0
-        if z%5==0
-          :fizzbuzz
-        else
-          :fizz
-        end          
-      elsif z%5==0
-        :buzz
-      else
-        z
-      end
-    end
+    # actually this is the method code I have posted, and the one that I had in the initial commit is
+    #    the result of my attempt to adhere to the suggestion in the comments:
+    #    fizzbuzz-а ти е сложен. Трите тернарни оператора са много гадни за четене. По-добре да го беше направил с if/else
+    #    I will change it again, as I dont like the potcome of the if/else approach
+    collect { |z| (z%3==0 and z%5==0) ? :fizzbuzz : (z%3==0 ? :fizz : (z%5==0 ? :buzz : z)) }
   end
 end
 
 class Hash
   def group_values
     result = {}
-    self.keys.each do |k|
-      unless result.has_key?(self.fetch(k))
-        result[self.fetch(k)]=[]
+    keys.each do |k|
+      unless result.has_key?(fetch(k))
+        result[fetch(k)]=[]
       end
-      result[self.fetch(k)]<<k  
+      result[fetch(k)] << k
     end
     result
   end
@@ -44,45 +37,6 @@ end
 
 class Array
   def densities
-    self.collect { |z| self.count(z) }
+    collect { |z| self.count(z) }
   end
 end
-
-p 0.prime_divisors
-p 1.prime_divisors
-p 4.prime_divisors
-p 10.prime_divisors
-p 36.prime_divisors
-p 42.prime_divisors
-p 17.prime_divisors
-p -23.prime_divisors
-num = -(2**31-1)
-p num.prime_divisors
-p num # check if object is replaced?
-puts "="*10
-
-
-p (1..6).fizzbuzz # [1, 2, :fizz, 4, :buzz, :fizz]
-p (13...100).fizzbuzz # last is 99
-range = (29..31)
-p range.fizzbuzz
-p range # to check if changed?
-puts "="*12
-
-
-p ({a: 1}).group_values             # {1 => [:a]}
-p ({a: 1, b: 2, c: 1}).group_values # {1 => [:a, :c], 2 => [:b]}
-hash = ({a: 5, b: 1, c: 2, baba: 1, dqdo: [], chushka: ({ami_sega: 1}), moje: 5, nqma: [],})
-p hash.group_values
-p hash
-puts "="*15
-
-
-p [:a, :b, :c].densities # [1, 1, 1]
-p [:a, :b, :a].densities # [2, 1, 2]
-p [:a, :a, :a].densities # [3, 3, 3]
-ary = [:a, [], nil, 55, 's',:a,:a,:a,[],[],nil,]+'sssssss'.split('')
-p ary.densities
-p ary
-
-
