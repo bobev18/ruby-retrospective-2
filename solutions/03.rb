c107424f38a6ca1592a363d15b8ce0d79d0870ec
Iliyan Bobev <bobev18@gmail.com>
2012-12-30 21:51:53 +0200

    implemented simplify in 03

--- solutions/03.rb
+++ solutions/03.rb
@@ -10,14 +10,12 @@ class Expr
 
   #--------------- remove -----------------
   def self.parse(input)
-    Expr.new ArithmeticParser.new.parse(input).to_sexp
+    new ArithmeticParser.new.parse(input).to_sexp
   end
   # ---------------------------------------
 
   def self.build(tree)
-    #initialize(
-    #@tree = tree
-    Expr.new tree
+    new tree
   end
 
   def ==(other)
@@ -36,12 +34,42 @@ class Expr
       when :cos      then Math.cos Expr.build(@tree[1]).evaluate(env)
     end
   end
+
+  def simplify
+    case @tree[0]
+      when :* then 
+        if Expr.build(@tree[1]).simplify==Expr.build([:number, 0]) or 
+            Expr.build(@tree[2]).simplify==Expr.build([:number, 0]) then
+            Expr.build([:number, 0])
+        elsif Expr.build(@tree[1]).simplify==Expr.build([:number, 1]) then
+          Expr.build(@tree[2]).simplify
+        elsif Expr.build(@tree[2]).simplify==Expr.build([:number, 1]) then
+          Expr.build(@tree[1]).simplify
+        else self
+        end
+      when :+ then 
+        if Expr.build(@tree[1]).simplify==Expr.build([:number, 0]) then Expr.build(@tree[2]).simplify
+        elsif Expr.build(@tree[2]).simplify==Expr.build([:number, 0]) then Expr.build(@tree[1]).simplify
+        else self
+        end
+      when :sin then Expr.build(@tree[1]).simplify == Expr.build([:number, 0]) ? Expr.build([:number, 0]) : @tree
+      else self
+    end 
+  end
+
+  def derive
+    #not implemented
+  end
+
 end
 
 expr = Expr.build([:+, [:number, 1], [:*, [:number, 2],[ :variable, :x]]])
 expr2 = Expr.build([:+, [:number, 1], [:*, [:number, 2], [:variable, :x]]])
+p "tree",expr.tree
 puts expr==expr2
 p expr.instance_of? Array
+p "tree",expr.tree
 p expr.evaluate(x: 2)
 p Expr.parse('x * 2 + y').evaluate(x: 3, y: 4) # => 10
 
+p Expr.parse('x + 0').simplify
